0039d56975098e0ce120892adc458fd1c09ce6eb
Erik Krogh Kristensen <erik@webbies.dk>
2015-09-08 23:19:09 +0200

    Getting git to recognize my damn class.

---
+++ src/dk/webbies/tscreate/paser/AST/ForInObjectStatement.java
@@ -0,0 +1,37 @@
+package dk.webbies.tscreate.paser.AST;
+
+import com.google.javascript.jscomp.parsing.parser.util.SourceRange;
+import dk.webbies.tscreate.paser.StatementVisitor;
+
+/**
+ * Created by Erik Krogh Kristensen on 07-09-2015.
+ */
+public class ForInObjectStatement extends Statement {
+    private final Statement initializer;
+    private final Expression collection;
+    private final Statement body;
+
+    public ForInObjectStatement(SourceRange loc, Statement initializer, Expression collection, Statement body) {
+        super(loc);
+        this.initializer = initializer;
+        this.collection = collection;
+        this.body = body;
+    }
+
+    public Statement getInitializer() {
+        return initializer;
+    }
+
+    public Expression getCollection() {
+        return collection;
+    }
+
+    public Statement getBody() {
+        return body;
+    }
+
+    @Override
+    public <T> T accept(StatementVisitor<T> visitor) {
+        return visitor.visit(this);
+    }
+}
