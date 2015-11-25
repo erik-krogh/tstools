6a7b8032fec307a6dfba83b120693556e24ae306
Erik Krogh Kristensen <erik@webbies.dk>
2015-11-25 22:28:32 +0100

    More frameworks to test

--- src/dk/webbies/tscreate/BenchMark.java
+++ src/dk/webbies/tscreate/BenchMark.java
@@ -1,14 +1,14 @@
 package dk.webbies.tscreate;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import static dk.webbies.tscreate.Main.*;
-import static dk.webbies.tscreate.Main.LanguageLevel.*;
-import static dk.webbies.tscreate.util.Util.*;
+import static dk.webbies.tscreate.Main.LanguageLevel;
+import static dk.webbies.tscreate.Main.LanguageLevel.ES5;
+import static dk.webbies.tscreate.Main.LanguageLevel.ES6;
+import static dk.webbies.tscreate.util.Util.evaluate;
 
 /**
  * Created by Erik Krogh Kristensen on 18-11-2015.
@@ -82,7 +82,44 @@ public class BenchMark {
         return new BenchMark("D3.js", "tests/d3/d3.js", null, options, ES5);
     });
 
-    // TODO: Way way way to much is a class. 
+    public static final BenchMark react = evaluate(() -> {
+        Options options = new Options();
+        return new BenchMark("React", "tests/react/react.js", null, options, ES5);
+    });
+
+    public static final BenchMark knockout = evaluate(() -> {
+        Options options = new Options();
+        return new BenchMark("Knockout", "tests/knockout/knockout.js", null, options, ES5);
+    });
+
+    // The stress test to rule all stress-tests. 10MB of JavaScript.
+    // TODO: Crashes in chrome.
+    public static final BenchMark ExtJS = evaluate(() -> {
+        Options options = new Options();
+        return new BenchMark("Ext JS", "tests/extjs/ext.js", null, options, ES5);
+    });
+
+    // TODO: Also crashes chrome.
+    public static final BenchMark ember = evaluate(() -> {
+        Options options = new Options();
+        return new BenchMark("Ember.js", "tests/ember/ember.js", null, options, ES5);
+    });
+
+    // TODO: One more that crashes chrome.
+    public static final BenchMark backbone = evaluate(() -> {
+        Options options = new Options();
+        return new BenchMark("Backbone.js", "tests/backbone/backbone.js", null, options, ES5);
+    });
+
+    // TODO: Runs in an infinite loop with createInstances.
+    // TODO: Lots of classes with duplicate names.
+    public static final BenchMark mooTools = evaluate(() -> {
+        Options options = new Options();
+        options.createInstances = false;
+        return new BenchMark("MooTools", "tests/mootools/mootools.js", null, options, ES5);
+    });
+
+    // TODO: Way way way to much is a class.
     public static final BenchMark prototype = evaluate(() -> {
         Options options = new Options();
         return new BenchMark("Prototype", "tests/prototype/prototype.js", null, options, ES5);
