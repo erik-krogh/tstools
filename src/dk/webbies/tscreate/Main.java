3b8e6045ce02f761efae16cc8bc15710f4d285e9
Erik Krogh Kristensen <erik@webbies.dk>
2015-12-04 14:23:10 +0100

    SSA Debug flag

--- src/dk/webbies/tscreate/Main.java
+++ src/dk/webbies/tscreate/Main.java
@@ -39,12 +39,21 @@ public class Main {
         System.exit(0);
     }
 
+    private static final boolean SSA_DEBUG = false;
+
     public static void runAnalysis(BenchMark benchMark) throws IOException {
         System.out.println("Analysing " + benchMark.name);
         String resultDeclarationFilePath = benchMark.scriptPath + ".gen.d.ts";
 
         String script = Util.readFile(benchMark.scriptPath);
-        FunctionExpression program = SSA.toSSA(new JavaScriptParser(benchMark.languageLevel).parse(benchMark.name, script).toTSCreateAST());
+
+        FunctionExpression program = new JavaScriptParser(benchMark.languageLevel).parse(benchMark.name, script).toTSCreateAST();
+
+        if (SSA_DEBUG) {
+            SSA.toSSA_(program); // Hamid entry-point.
+
+            return;
+        }
 
         Snap.Obj globalObject = JSNAPUtil.getStateDump(JSNAPUtil.getJsnapRaw(benchMark.scriptPath, benchMark.options, benchMark.dependencyScripts()), program);
 
