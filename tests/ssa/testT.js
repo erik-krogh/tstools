968db91c6c75ad7914bb2993da92dbff9c69af47
Hamid A. Toussi <hamid@cs.au.dk>
2015-12-29 19:55:35 +0100

    ssa tests

--- tests/ssa/testT.js
+++ tests/ssa/testT.js
@@ -1,2 +1,4 @@
 var fc = 0, x = 0,y=0;
 fc(x, y);
+z=y +fc(3,4);
+q=x.ff(4);
---
+++ tests/ssa/th.js
@@ -0,0 +1,10 @@
+var x = 800;
+throw 42;
+y=x + 420;
+if (y) {
+    x = x + 1;
+} else {
+    //throw 52;
+    x = y + 100;
+}
+z =x + 1000;
