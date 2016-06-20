a1bb942ddfeb8f01faa1f87f3fbf309ae5c36eb1
Erik Krogh Kristensen <erik@webbies.dk>
2016-06-20 14:56:32 +0200

    Refac

--- diffViewer/app.js
+++ diffViewer/app.js
@@ -18,109 +18,122 @@ var app = angular.module('materializeApp', ['ui.materialize'])
         }
 
     }]).controller('StatementController', ["$scope", "$http", function ($scope, $http) {
-        
+
         $scope.show = {
-            all : false
+            all: false
+        };
+
+        var statementTypes = {
+            changedType: {
+                getTitle: function (statement) {
+                    return "Type changed on " + statement.typePath;
+                },
+                getCodeSamples: function (statement) {
+                    return [
+                        {
+                            description: "Type changed from: ",
+                            code: statement.oldType
+                        }, {
+                            description: "To: ",
+                            code: statement.newType
+                        },
+                        {
+                            description: "In container: ",
+                            code: statement.containerType
+                        }
+                    ]
+                }
+            },
+            addedProperty: {
+                getTitle: function (statement) {
+                    if (statement.isClass) {
+                        return "Static property \"" + statement.key + "\" added to " + statement.containerDescription + " on " + statement.typePath;
+                    } else {
+                        return "Property \"" + statement.key + "\" added to " + statement.containerDescription + " on " + statement.typePath;
+                    }
+                },
+                getCodeSamples: function (statement) {
+                    var result = [];
+                    if (statement.isAny) {
+                        result.push({
+                            description: "A type could not be inferred"
+                        })
+                    } else {
+                        result.push({
+                            description: "Type: ",
+                            code: statement.newType
+                        })
+                    }
+                    result.push({
+                        description: "In container: ",
+                        code: statement.containerType
+                    });
+                    return result;
+                }
+            },
+            removedProperty: {
+                getTitle: function (statement) {
+                    if (statement.isClass) {
+                        return "Static property \"" + statement.key + "\" removed from " + statement.containerDescription + " on " + statement.typePath;
+                    } else {
+                        return "Property \"" + statement.key + "\" removed from " + statement.containerDescription + " on " + statement.typePath;
+                    }
+                },
+                getCodeSamples: function (statement) {
+                    return [{
+                        description: "New container type: ",
+                        code: statement.containerType
+                    }]
+                }
+            },
+            changedArgCount: {
+                getTitle: function (statement) {
+                    if (statement.isClass) {
+                        return "Number of arguments changed from " + statement.oldArgCount + " to " + statement.newArgCount + " in constructor for " + statement.newTypeDescription + " on " + statement.typePath;
+                    } else {
+                        return "Number of arguments changed from " + statement.oldArgCount + " to " + statement.newArgCount + " on " + statement.typePath;
+                    }
+                },
+                getCodeSamples: function (statement) {
+                    return [
+                        {
+                            description: "Old function: ",
+                            code: statement.oldType
+                        }, {
+                            description: "New function: ",
+                            code: statement.newType
+                        }]
+                }
+            }
         };
 
         $scope.getStatementTitle = function (statement) {
-            if (statement.type == "changedType") {
-                return "Type changed on " + statement.typePath;
-            } else if (statement.type == "addedProperty") {
-                if (statement.isClass) {
-                    return "Static property \"" + statement.key + "\" added to " + statement.containerDescription + " on " + statement.typePath;
-                } else {
-                    return "Property \"" + statement.key + "\" added to " + statement.containerDescription + " on " + statement.typePath;
-                }
-            } else if (statement.type == "removedProperty") {
-                if (statement.isClass) {
-                    return "Static property \"" + statement.key + "\" removed from " + statement.containerDescription + " on " + statement.typePath;
-                } else {
-                    return "Property \"" + statement.key + "\" removed from " + statement.containerDescription + " on " + statement.typePath;
-                }
-            } else if (statement.type == "changedArgCount") {
-                if (statement.isClass) {
-                    return "Number of arguments changed from " + statement.oldArgCount + " to " + statement.newArgCount + " in constructor for " + statement.newTypeDescription + " on " + statement.typePath;
-                } else {
-                    return "Number of arguments changed from " + statement.oldArgCount + " to " + statement.newArgCount + " on " + statement.typePath;
-                }
-            } else {
-                console.log(statement);
-            }
+            return statementTypes[statement.type].getTitle(statement);
         };
 
         $scope.getCodeSamples = function (statement) {
-            var result = [];
-            if (statement.type == "changedType") {
-                result.push({
-                    description: "Type changed from: ",
-                    code: statement.oldType
-                });
-                result.push({
-                    description: "To: ",
-                    code: statement.newType
-                });
-                result.push({
-                    description: "In container: ",
-                    code: statement.containerType
-                })
-            } else if (statement.type == "addedProperty") {
-                if (statement.isAny) {
-                    result.push({
-                        description: "A type could not be inferred"
-                    })
-                } else {
-                    result.push({
-                        description: "Type: ",
-                        code: statement.newType
-                    })
-                }
-                result.push({
-                    description: "In container: ",
-                    code: statement.containerType
-                })
-            } else if (statement.type == "removedProperty") {
-                result.push({
-                    description: "New container type: ",
-                    code: statement.containerType
-                });
-            } else if (statement.type == "changedArgCount") {
-                result.push({
-                    description: "Old function: ",
-                    code: statement.oldType
-                });
-                result.push({
-                    description: "New function: ",
-                    code: statement.newType
-                })
-            } else {
-                console.log(statement);
-            }
-            if (result.length == 0) {
-                console.error("Didn't get any code samples. ")
-            }
-            return result;
+            return statementTypes[statement.type].getCodeSamples(statement);
         };
 
     }]);
 
 
 // Thanks to: http://stackoverflow.com/questions/22742899/using-prismjs-in-angular-app#answer-24697575
-app.directive('nagPrism', ['$compile', function($compile) {
+app.directive('nagPrism', ['$compile', function ($compile) {
     return {
         restrict: 'A',
         transclude: true,
         scope: {
             source: '@'
         },
-        link: function(scope, element, attrs, controller, transclude) {
-            scope.$watch('source', function(v) {
+        link: function (scope, element, attrs, controller, transclude) {
+            scope.$watch('source', function (v) {
                 element.find("code").html(v);
 
                 Prism.highlightElement(element.find("code")[0]);
             });
 
-            transclude(function(clone) {
+            transclude(function (clone) {
                 if (clone.html() !== undefined) {
                     element.find("code").html(clone.html());
                     $compile(element.contents())(scope.$parent);
