65efcd635f638b004c7acd466b9a08a715be77f8
Erik Krogh Kristensen <erik@webbies.dk>
2016-10-10 14:32:31 +0200

    Converting to a "npm install" installation

--- initialize.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-git submodule update --init --recursive;
-cd lib/jsnap/;
-npm install;
-cd ../../;
-cd lib/ts-type-reader/;
-npm install;
-node_modules/typescript/bin/tsc --module commonjs src/*.ts;
-cd ../../;
